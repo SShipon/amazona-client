71bb65fbdcbbebbd404e02e3040e129020e9345b
SShipon <nxshipon@gmail.com>
2025-01-01 12:50:23 +0600

    select input add

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME= process.env.NEXT_PUBLIC_APP_NAME || "NExtAMzon"
+export  const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Seped less enjoy more"
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "An amazon clone built with next.js and mongoDB"
